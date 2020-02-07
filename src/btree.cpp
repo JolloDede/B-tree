e30938fd8544b7646035fc8bf808fcf2627bc03b
Gitea <gitea@fake.local>
2020-02-07 16:33:37 +0100

    Error commit

---
+++ src/btree.cpp
@@ -0,0 +1,5 @@
+#include "btree.h"
+
+BTree::BTree(){
+    std::cout << "Hallo Class";
+}
---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "..\src\btree.h"
+
+using namespace std;
+
+int main(){
+    cout << "hello World";
+    BTree tree();
+}
